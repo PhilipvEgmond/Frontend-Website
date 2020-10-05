0c34b20319523c9058495fe3be8b134258aec7c8
PhilipvEgmond <70648723+PhilipvEgmond@users.noreply.github.com>
2020-10-05 17:25:57 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+
+// Laat pagina zien als het geladen is
+var bodyElement = document.querySelector("body");
+var flashElement = document.querySelector("body span:nth-of-type(1)"); 
+
+function fade() {
+    flashElement.classList.add("fade"); 
+}
+
+window.addEventListener("load", fade);
